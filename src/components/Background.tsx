d1398d59d2793f14ae91fbe06026a99ea4c52e72
Rafael Castro <ralphc1992@live.com>
2024-07-29 22:59:32 -0500

    fixed background resolved

--- src/components/Background.tsx
+++ src/components/Background.tsx
@@ -1,9 +1,9 @@
 import React from "react";
-
+import pcRoom from "../assets/pc-room.jpg";
 export const Background: React.FC = () => {
   return (
-    <div className="relative z-0">
-      <div className="fixed top-0 left-0 w-full h-[100vh] z-0 opacity-[10%] blur-sm img-bg"></div>
+    <div className="fixed top-0 left-0 w-full h-[100vh] z-10 opacity-[10%] blur-sm">
+      <img src={pcRoom} alt="" />
     </div>
   );
 };
