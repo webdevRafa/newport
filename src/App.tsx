5dc8ddb04a10b82ba5952926218f8984bd518fd6
Rafael Castro <ralphc1992@live.com>
2024-07-29 21:39:12 -0500

    adding cool pic

--- src/App.tsx
+++ src/App.tsx
@@ -8,12 +8,11 @@ function App() {
       <Tech />
       {/* background image fixed */}
       <div className="relative z-0">
-        <div className="fixed top-0 left-0 w-full h-[100vh] z-0 opacity-10 bgtester"></div>
+        <div className="fixed top-0 left-0 w-full h-[100vh] z-0 opacity-10 img-bg"></div>
       </div>
-      <div className="w-full h-[400px] coffee"></div>
+      <div className="max-w-[1600px] w-[90%] shadow-md h-[400px] mx-auto coffee z-40 relative"></div>
       <Contact />
       <div className="h-[600px]"></div>
-      <div className="w-full h-[400px] underrated"></div>
       <div className="h-[1000px]"></div>
     </>
   );
